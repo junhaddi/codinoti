f4e253c0f829aebdf1124b8a2c92d77609794725
junhaddi <rkdwnsgk05@gmail.com>
2020-03-19 05:30:05 +0900

    웹 푸시 구현

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("./fcm-cloud-messaging-test-e4860-firebase-adminsdk-20v2k-1a03eb9ec3.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fcm-cloud-messaging-test-e4860.firebaseio.com"
+});
+
 var express = require("express");
 var app = express();
 app.use(express.json());
@@ -10,11 +17,37 @@ app.get("/", function(req, res) {
 
 app.post("/token", function(req, res) {
   var data = req.body.token;
-  console.log(`tokken register: ${data}`);
-  res.send({ result: "submit tokken" });
+  tokenList.push(data);
+  console.log(`token register: ${data}`);
+  res.send({ result: "submit token" });
 });
 
 var server = app.listen(8000, function() {
   var port = server.address().port;
   console.log(`app listening on port ${port}`);
 });
+
+// 푸시 메시지
+var tokenList = [];
+
+setInterval(function() {
+  if (tokenList.length > 0) {
+    var message = {
+      notification: {
+        body: "Background Message",
+        title: "BG Title"
+      },
+      data: {
+        title: "FG Title",
+        message: "Foreground Message"
+      },
+      tokens: tokenList
+    };
+    admin
+      .messaging()
+      .sendMulticast(message)
+      .then(response => {
+        console.log(response.successCount + " messages were sent successfully");
+      });
+  }
+}, 3000);
